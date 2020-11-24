54c2f807a9add1bb3f92de11be286bcd7af63baa
zovivo <tmczovivo@gmail.com>
2020-11-24 21:30:34 +0700

    maybe last-commit :(

--- main/js/app.js
+++ main/js/app.js
@@ -1,84 +1,39 @@
 URL = window.URL || window.webkitURL;
 
-var gumStream; 						//stream from getUserMedia()
-var rec; 							//Recorder.js object
-var input; 							//MediaStreamAudioSourceNode we'll be recording
+var gumStream;
+var rec;
+var input;
 
-// shim for AudioContext when it's not avb. 
 var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext //audio context to help us record
-
+var audioContext
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
 var pauseButton = document.getElementById("pauseButton");
+var translateButton = document.getElementById("translate");
+var fileRecord;
 
 
-//add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
 pauseButton.addEventListener("click", pauseRecording);
+translateButton.addEventListener("click", translateClick);
 
-$().ready(function () {
-	console.log("ready!");
-	$( ".myClass" ).hide();
-});
 
 function startRecording() {
-	console.log("recordButton clicked");
-
-	/*
-		Simple constraints object, for more advanced audio features see
-		https://addpipe.com/blog/audio-constraints-getusermedia/
-	*/
-
 	var constraints = { audio: true, video: false }
-
-	/*
-	  Disable the record button until we get a success or fail from getUserMedia() 
-  */
-
 	recordButton.disabled = true;
 	stopButton.disabled = false;
 	pauseButton.disabled = false
-
-	/*
-		We're using the standard promise based getUserMedia() 
-		https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-	*/
-
 	navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
-
-		/*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
-
-		*/
 		audioContext = new AudioContext();
-
-		//update the format 
 		document.getElementById("formats").innerHTML = "Format: 1 channel pcm @ " + audioContext.sampleRate / 1000 + "kHz"
-
-		/*  assign to gumStream for later use  */
 		gumStream = stream;
-
-		/* use the stream */
 		input = audioContext.createMediaStreamSource(stream);
-
-		/* 
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
 		rec = new Recorder(input, { numChannels: 1 })
-
-		//start the recording process
 		rec.record()
 
-		console.log("Recording started");
 
 	}).catch(function (err) {
-		//enable the record button if getUserMedia() fails
 		recordButton.disabled = false;
 		stopButton.disabled = true;
 		pauseButton.disabled = true
@@ -86,108 +41,72 @@ function startRecording() {
 }
 
 function pauseRecording() {
-	console.log("pauseButton clicked rec.recording=", rec.recording);
 	if (rec.recording) {
-		//pause
 		rec.stop();
 		pauseButton.innerHTML = "Resume";
 	} else {
-		//resume
 		rec.record()
 		pauseButton.innerHTML = "Pause";
 
 	}
 }
 
-function showImages(locationName) {
-	$( ".myClass" ).hide();
-	if (locationName == "Hoàn Kiếm") {
-		$( ".myClass" ).show();
+function showImages(listLocation) {
+	$("#listImages").empty();
+	if (listLocation.length > 0) {
+		var listImage = "";
+		listLocation.map((locationImage) => {
+			listImage += '<div class="media"><a class= "myClass" href = "' + locationImage + '" > <img src="' + locationImage + '" alt="" title="This right here is a caption." /></a></div > '
+		});
+		$('#listImages').append(listImage);
 	}
 }
 
 function stopRecording() {
-	console.log("stopButton clicked");
-
-	//disable the stop button, enable the record too allow for new recordings
 	stopButton.disabled = true;
 	recordButton.disabled = false;
 	pauseButton.disabled = true;
-
-	//reset button just in case the recording is stopped while paused
 	pauseButton.innerHTML = "Pause";
-
-	//tell the recorder to stop the recording
 	rec.stop();
-
-	//stop microphone access
 	gumStream.getAudioTracks()[0].stop();
-
-	//create the wav blob and pass it on to createDownloadLink
-	rec.exportWAV(createDownloadLink);
+	rec.exportWAV(createRecordControl);
 }
 
-function createDownloadLink(blob) {
+function translateClick() {
+	var result = $("#resultTranslate");
+	result.val("");
+	$.ajax({
+		url: 'https://demo-translate-zovivo.herokuapp.com/translation/read-file',
+		type: 'post',
+		dataType: 'json',
+		cache: false,
+		contentType: false,
+		processData: false,
+		data: fileRecord,
+		success: function (data) {
+			if (data) {
+				result.val(data.data.result);
+				showImages(data.data.images);
+			}
+		},
+	});
+}
+
+function createRecordControl(blob) {
 
 	var url = URL.createObjectURL(blob);
 	var au = document.createElement('audio');
 	var li = document.createElement('li');
 	var link = document.createElement('a');
-
-	//name of .wav file to use during upload and download (without extendion)
 	var filename = 'record';
-
-	//add controls to the <audio> element
 	au.controls = true;
 	au.src = url;
-
-	//save to disk link
 	link.href = url;
-	link.download = filename + ".wav"; //download forces the browser to donwload the file using the  filename
-	//link.innerHTML = "Save to disk";
-
-	//add the new audio element to li
+	link.download = filename + ".wav";
 	li.appendChild(au);
-
-	//add the filename to the li
-	//li.appendChild(document.createTextNode(filename + ".wav "))
-
-	//add the save to disk link to li
-	//li.appendChild(link);
-
-	//upload lin
-	var upload = document.getElementById("translate");
-	// var upload = document.createElement('a');
-	// upload.href = "#";
-	// upload.innerHTML = "Translate";
-	upload.addEventListener("click", function (event) {
-		event.preventDefault()
-		var result = $("#resultTranslate");
-		result.innerHTML = "Waiting for result";
-		var fd = new FormData();
-		fd.append("fileRecord", blob, filename);
-		$.ajax({
-			url: 'https://demo-translate-zovivo.herokuapp.com/translation/read-file',
-			type: 'post',
-			dataType: 'json',
-			cache: false,
-			contentType: false,
-			processData: false,
-			data: fd,
-			success: function (data) {
-				if (data) {
-					console.log(data);
-					result.val(data.data);
-					showImages(data.data);
-				}
-			},
-		});
-
-
-	})
-	li.appendChild(document.createTextNode(" "))//add a space in between
-	// li.appendChild(upload)//add the upload link to li
-
-	//add the li element to the ol
+	var fd = new FormData();
+	fd.append("fileRecord", blob, filename);
+	fileRecord = fd;
+	li.appendChild(document.createTextNode(" "))
 	recordingsList.appendChild(li);
 }
