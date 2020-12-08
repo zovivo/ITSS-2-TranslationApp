02d7b4cdb1fb906c03ceb14831ba9a2ea4f2ab26
sogun1998 <nguyenducanh26898@gmail.com>
2020-12-08 23:54:08 +0700

    Update v2

--- app/image.js
+++ app/image.js
@@ -7,7 +7,48 @@
     let model, webcam, labelContainer, maxPredictions;
 
     // Load the image model and setup the webcam
-    
+    async function init() {
+        const modelURL = URL + 'model.json';
+        const metadataURL = URL + 'metadata.json';
+
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // or files from your local hard drive
+        model = await tmImage.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+
+        // Convenience function to setup a webcam
+        const flip = true; // whether to flip the webcam
+        webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        webcam.play();
+        window.requestAnimationFrame(loop);
+
+        // append elements to the DOM
+        document.getElementById('webcam-container').appendChild(webcam.canvas);
+        labelContainer = document.getElementById('label-container');
+        for (let i = 0; i < maxPredictions; i++) { // and class labels
+            labelContainer.appendChild(document.createElement('div'));
+        }
+    }
+
+    async function loop() {
+        webcam.update(); // update the webcam frame
+        await predict();
+        window.requestAnimationFrame(loop);
+    }
+
+    // run the webcam image through the image model
+    async function predict() {
+        // predict can take in an image, video or canvas html element
+        
+        const prediction = await model.predict(webcam.canvas);
+        for (let i = 0; i < maxPredictions; i++) {
+            const classPrediction =
+                prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
+            labelContainer.childNodes[i].innerHTML = classPrediction;
+        }
+    }
     function readURL(input) {
         if (input.files && input.files[0]) {
           var reader = new FileReader();
@@ -20,6 +61,7 @@
           reader.readAsDataURL(input.files[0]);
         }
       }
+    
     async function go() {
         const modelURL = URL + 'model.json';
         const metadataURL = URL + 'metadata.json';
@@ -53,7 +95,7 @@
         const prediction = await model.predict(img);
         for (let i = 0; i < maxPredictions; i++) {
             const classPrediction =
-				prediction[i].className + ': ' + prediction[i].probability.toFixed(2) * 100 + '%';
+                prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
             labelContainer.childNodes[i].innerHTML = classPrediction;
         }
     }
